0e5d05fabf4440f7ebd81fdf81927625ab472551
Lindsay Esterman <lindsayesterman@gmail.com>
2020-12-20 17:27:15 -0500

    reindentation

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-'use strict'
-function seeSamples(){
-	$(".here").click(event=>{
-	$(".flex").toggle(450);
-})}
+"use strict";
+function seeSamples() {
+  $(".here").click((event) => {
+    $(".flex").toggle(450);
+  });
+}
 
 seeSamples();
 
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-
+$(document).ready(function () {
+  $("a").on("click", function (event) {
     if (this.hash !== "") {
       event.preventDefault();
 
       var hash = this.hash;
 
-      
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 450, function(){
-   
-     
-        window.location.hash = hash;
-      });
-    } 
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        450,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
   });
 });
